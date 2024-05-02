baa1155cec3f81b2237e5c44987cda37f536dd7e
Neopolitan <neoyukiaylor@gmail.com>
2024-05-02 19:33:56 +0100

    Preperation for download

---
+++ NoMorePolePlants/NoMorePolePlantsMain.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+
+namespace NoMorePolePlants
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class NoMorePolePlantsMain : BaseUnityPlugin {
+
+        public const string PLUGIN_GUID = "NeoNyaa.NoMorePolePlants";
+        public const string PLUGIN_NAME = "No More Pole Plants";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        public void OnEnable() {
+            On.PoleMimic.Initiate += PoleMimicInitHook;
+            On.PoleMimic.Update += PoleMimicUpdateHook;
+        }
+
+        void PoleMimicInitHook(On.PoleMimic.orig_Initiate orig, PoleMimic self) {
+            
+        }
+
+        void PoleMimicUpdateHook( On.PoleMimic.orig_Update orig, PoleMimic self, bool eu ) {
+
+        }
+    }
+}

cd1b9f8285665dbf890695bbc8ae8ac982799e26
Neopolitan <neoyukiaylor@gmail.com>
2024-05-02 20:03:06 +0100

    Fixed the mod not working For some reason release versions dont work whereas debug versions do.

--- NoMorePolePlants/Properties/AssemblyInfo.cs
+++ NoMorePolePlants/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 using System.Security.Permissions;
 
 // General Information about an assembly is controlled through the following
